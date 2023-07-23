54cfdb2573ebab1eb53495ae791cd9b608c72902
Chris Marshall <thoughtpalettechris@gmail.com>
2023-07-22 23:22:15 -0500

    removed postcss for twind due to working better with global tailwind classes in web components

--- src/components/app-container.ts
+++ src/components/app-container.ts
@@ -1,13 +1,16 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import install from '@twind/with-web-components'
+import config from '../../twind.config'
 
 @customElement('app-container')
+@install(config)
 export class AppContainer extends LitElement {
     @property() firstName = 'chris'
 
-    render() {
+    override render() {
         return html`
-            <main class="container">yeet</main>
+            <main class="container mx-auto">yeet</main>
         `
     }
 }
